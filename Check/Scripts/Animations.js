58273daec777c9bf48e1e3b26a23c81c0788bb0c
Sebastian <cappadonnasebastian10@gmail.com>
2021-03-02 23:30:45 -0600

    Housing Selection Page created

--- Check/Scripts/Animations.js
+++ Check/Scripts/Animations.js
@@ -5,3 +5,19 @@ sr.reveal('.modal', {
     origin: 'top',
     distance: '-100px'
 });
+
+
+
+window.sr = ScrollReveal();
+sr.reveal('.hSelection', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.p-1', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-50px'
+});
---
+++ Check/Views/HousingSelectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Check.Views
+{
+    public class HousingSelectController : Controller
+    {
+        // GET: HousingSelect
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
